fdb938c03192d62bbce85a35994a38780b9ff64b
Marvin Pohl <mp078@hdm-stuttgart.de>
2015-09-24 23:14:48 +0200

    Removing empty tags on parse

--- Catch-GUI/CatchTest.cpp
+++ Catch-GUI/CatchTest.cpp
@@ -16,7 +16,16 @@ CatchTest CatchTest::ParseFromCommandLine(const QStringList &cmd)
     QStringList tags;
     if(cmd[1].startsWith("    "))
     {
-        tags = cmd[1].split(QRegExp("[\\[\\]]"));
+        tags = cmd[1].trimmed().split(QRegExp("[\\[\\]]"));
+        QMutableStringListIterator i(tags);
+        while(i.hasNext())
+        {
+            QString cur = i.next();
+            if(cur.isEmpty())
+            {
+                i.remove();
+            }
+        }
     }
     return CatchTest(name, tags);
 }
